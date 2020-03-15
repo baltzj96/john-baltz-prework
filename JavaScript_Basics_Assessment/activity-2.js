06a5fe7877212e3788fc1bdab9026d9e094bd5de
baltzj96 <john.baltz55@gmail.com>
2020-03-15 01:10:37 -0500

    module-2 assessment

--- JavaScript_Basics_Assessment/activity-2.js
+++ JavaScript_Basics_Assessment/activity-2.js
@@ -4,5 +4,5 @@ const prod=9*9; console.log(prod); // console output
 const quo=56/12; console.log(quo); // console output
 const rem= 281%9; console.log(rem); // console output
 const undef= 18/0; console.log(undef); // console output
-const string1= '56'; const string2='92'; console.log(string1  +  string2); // console output
+const string1= '56'; const string2='92'; console.log(string1 + string2); console.log('${string1} ${string2}'); // console output
 console.log(52===42); // console output
